cde780b7ab6d628f67383d796899b0d0340fcec6
Andrei Chelariu <andreichelariu92@yahoo.com>
2018-04-15 16:28:26 +0300

    Perform GET request if the POST request for the granting ticket succeeded.

--- ticket_service/lua_src/http_client_test.lua
+++ ticket_service/lua_src/http_client_test.lua
@@ -1,21 +1,54 @@
+require("base64")
+
 local http_request = require("http.request")
 local http_tls = require("http.tls")
-local context = http_tls:new_client_context()
 local openssl_ctx = require "openssl.ssl.context"
 
+--Create SSL context that accepts self signed certificates.
+local context = http_tls:new_client_context()
 context:setVerify(openssl_ctx.VERIFY_NONE)
-
-local request = http_request.new_from_uri("http://localhost:8080/TicketService/GrantingTickets")
-request.ctx = context
-request.tls = true
-request.headers:upsert(":method", "POST")
-request.headers:append("content-type", "application/x-www-form-urlencoded")
-request:set_body("user=andreichelariu&pass=sex")
-
-local headers, stream =request:go(100)
-print("Headers ", headers, "stream ", stream)
+--Perform POST request to create a new granting ticket.
+local getRequest = http_request.new_from_uri("https://localhost:8080/TicketService/GrantingTickets")
+getRequest.ctx = context
+getRequest.tls = true
+getRequest.headers:upsert(":method", "POST")
+getRequest.headers:append("content-type", "application/x-www-form-urlencoded")
+getRequest:set_body("user=andreichelariu&pass=blah")
+local headers, stream =getRequest:go(100)
+--Save the URI of the new resource.
+local resourceUri = nil
 if headers then
+    print("Headers of POST request: ")
     for k, v in headers:each() do
+        if k == "content-location" then
+            resourceUri = v
+        end
         print(k, v)
     end
+end
+
+if not resourceUri then
+    print("Error creating resource")
+    return
+end
+
+--Perform GET request on the newly created granting ticket.
+local getRequest = http_request.new_from_uri("https://localhost:8080" .. resourceUri)
+getRequest.ctx = context
+getRequest.tls = true
+local userPass = toBase64("andreichelariu:blah")
+getRequest.headers:upsert(":method", "GET")
+getRequest.headers:upsert("authorization","Basic " .. userPass)
+local getHeaders, getStream = getRequest:go(100)
+--Print headers of the request
+print("Get headers ", getHeaders, " getStream ", getStream)
+if getHeaders then
+    print("Headers of GET request: ")
+    for k, v in getHeaders:each() do
+        print(k, v)
+    end
+end
+--Print the body of the request (html representation of the granting ticket).
+if getStream then
+    print(getStream:get_body_as_string())
 end
