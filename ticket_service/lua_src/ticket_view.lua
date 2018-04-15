5e7e8b200af12af335c0c521bc4ccde68e042d06
Andrei Chelariu <andreichelariu92@yahoo.com>
2018-04-15 16:24:51 +0000

    Show owner of granting ticket.

--- ticket_service/lua_src/ticket_view.lua
+++ ticket_service/lua_src/ticket_view.lua
@@ -6,11 +6,13 @@ function renderGrantingTicket(gt)
         <p id="key">%s</p>
         <p id="iv">%s</p>
         <p id="blob">%s</p>
+	<p id="owner">%s</p>
     </body>
     </html>]],
     toHex(gt.key),
     toHex(gt.IV),
-    toHex(gt.blob))
+    toHex(gt.blob),
+    gt.user)
 
     return html
 end
